4c1ec81cb61960093bd7c0b7713eb2dc298c2f44
neelkanth18-vk <neelkandh123@gmail.com>
2026-07-15 12:48:49 +0530

    Print DATABASE_URL

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from core.config import settings
+import sys
 
-print("DATABASE_URL =", repr(settings.DATABASE_URL))
+print("DATABASE_URL START")
+print(repr(settings.DATABASE_URL))
+print("DATABASE_URL END")
+sys.stdout.flush()
 
 engine = create_engine(settings.DATABASE_URL)
 
