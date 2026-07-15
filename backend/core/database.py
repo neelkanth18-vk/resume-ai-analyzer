8742e511be4db0c45469adf0ee5cc5ede9a3a0e5
neelkanth18-vk <neelkandh123@gmail.com>
2026-07-15 13:04:50 +0530

    Remove debug code

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,22 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from core.config import settings
-import sys
-from sqlalchemy.engine.url import make_url
-
-url = make_url(settings.DATABASE_URL)
-
-print("Driver:", url.drivername)
-print("Host:", url.host)
-print("Port:", url.port)
-print("Database:", url.database)
-
-engine = create_engine(settings.DATABASE_URL)
-
-print("DATABASE_URL START")
-print(repr(settings.DATABASE_URL))
-print("DATABASE_URL END")
-sys.stdout.flush()
 
 engine = create_engine(settings.DATABASE_URL)
 
